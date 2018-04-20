be5d00b42187f262926a84ac809b4ad380c419a1
viki1206 <viki120698@gmail.com>
2018-04-20 19:34:18 +0200

    b

--- 4.feladat.py
+++ 4.feladat.py
@@ -4,6 +4,11 @@ def pig_latin(szoveg):
     for ch in szoveg:
         if 'a'<=ch<='z':
             seged+=ch
-    print(seged)
+        elif len(seged) != 0:
+            for i in range(1, len(seged)):
+                szoveg2 += seged[i]
+            szoveg2 += seged[0] + 'ay' + ch
+            seged = ''
+    print(szoveg2)
 
 pig_latin('az a kecske egy nyul')
