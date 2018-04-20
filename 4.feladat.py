773e90aaaf90290dadc50a1f543aac6b66b1329b
viki1206 <viki120698@gmail.com>
2018-04-20 19:47:29 +0200

    b

--- 4.feladat.py
+++ 4.feladat.py
@@ -1,14 +1,24 @@
-def pig_latin(szoveg):
+def pig_latin(a):
     seged=''
-    szoveg2=''
-    for ch in szoveg:
-        if 'a'<=ch<='z':
+    b=''
+    for ch in a:
+        if 'A' <= ch <= 'z':
             seged+=ch
-        elif len(seged) != 0:
-            for i in range(1, len(seged)):
-                szoveg2 += seged[i]
-            szoveg2 += seged[0] + 'ay' + ch
-            seged = ''
-    print(szoveg2)
+        elif len(seged)!= 0:
+            for i in range(1,len(seged)):
+                b+=seged[i]
+            b+=seged[0]+'ay'+ch
+            seged=''
 
-pig_latin('az a kecske egy nyul')
+
+    for i in range(1, len(seged)):
+        b += seged[i]
+    for i in seged:
+        b += i
+        break
+    b += 'ay'
+    b=b.capitalize()
+    print(b)
+    return 0
+
+pig_latin("The quick brown fox")
