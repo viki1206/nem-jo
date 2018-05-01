371b00f79b0661798772cad7d03cd594567c64d9
viki1206 <viki120698@gmail.com>
2018-05-01 20:13:28 +0200

    1

--- 3.feladat.py
+++ 3.feladat.py
@@ -1,10 +1,12 @@
 def keret(a):
     b=a.split( )
+    maximum=0
+    for i in range(0,len(b)):
     for i in range(0, len(b)):
         help = '*'
         help += b[i]
         print(help)
-    
+
 
 
 keret('A macska szereti  a tejet')
