3eb0cab57d5ae3d73c4e26ab45857ed7269fc397
viki1206 <viki120698@gmail.com>
2018-05-01 20:15:00 +0200

    1

--- 3.feladat.py
+++ 3.feladat.py
@@ -2,6 +2,8 @@ def keret(a):
     b=a.split( )
     maximum=0
     for i in range(0,len(b)):
+        if ((b[i]) > maximum):
+            maximum = len(b[i])
     for i in range(0, len(b)):
         help = '*'
         help += b[i]
